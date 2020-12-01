14549049a6a2826c251a9c6457625b05c5098788
Muhammad5776 <72851181+Muhammad5776@users.noreply.github.com>
2020-12-01 20:11:11 +0500

    edit New Year

---
+++ new year/js/main.js
@@ -0,0 +1,101 @@
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
+var btn4 = document.getElementById('btn4');
+var btn5 = document.getElementById('btn5');
+var btn6 = document.getElementById('btn6');
+var btn7 = document.getElementById('btn7');
+var btn8 = document.getElementById('btn8');
+var btn9 = document.getElementById('btn9');
+var btn10 = document.getElementById('btn10');
+var btn11 = document.getElementById('btn11');
+var btn12 = document.getElementById('btn12');
+var btn13 = document.getElementById('btn13');
+var btn14 = document.getElementById('btn14');
+var btn15 = document.getElementById('btn15');
+var btn16 = document.getElementById('btn16');
+var btn17 = document.getElementById('btn17');
+var btn18 = document.getElementById('btn18');
+var btn19 = document.getElementById('btn19');
+var btn20 = document.getElementById('btn20');
+var img = document.getElementById('img');
+btn1.addEventListener('click', () => {
+  img.src = './images/money.png'
+  document.getElementById('result').innerHTML = "";
+});
+btn2.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Omad";
+});
+btn3.addEventListener('click', () => {
+  img.src = './images/iWatch.png'
+  document.getElementById('result').innerHTML = "";
+});
+btn4.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Sog'lik";
+});
+btn5.addEventListener('click', () => {
+  img.src = './images/iPhone.png'
+  document.getElementById('result').innerHTML = "";
+});
+btn6.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Baxt.";
+});
+btn7.addEventListener('click', () => {
+  img.src = './images/friends.png'
+  document.getElementById('result').innerHTML = "Hazilkash do'stlar.";
+});
+btn8.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Yangi uy.";
+});
+btn9.addEventListener('click', () => {
+  img.src = './images/iPad.png'
+  document.getElementById('result').innerHTML = "";
+});
+btn10.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Talaba bo'lish baxti.";
+});
+btn11.addEventListener('click', () => {
+  img.src = './images/banana.png'
+  document.getElementById('result').innerHTML = "";
+});
+btn12.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Tinchlik.";
+});
+btn13.addEventListener('click', () => {
+  img.src = './images/ticket.png'
+  document.getElementById('result').innerHTML = "Haj safariga 2 kishilik chipta";
+});
+btn14.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Yangi yilni oila bag'rida kutish.";
+});
+btn15.addEventListener('click', () => {
+  img.src = './images/dog.png'
+  document.getElementById('result').innerHTML = "";
+});
+btn16.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Qorboboni yaxshi bolalar ro'yxatiga kirish.";
+});
+btn17.addEventListener('click', () => {
+  img.src = './images/car.png'
+  document.getElementById('result').innerHTML = "";
+});
+btn18.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Yangi yilni yolg'iz o'tkazish";
+});
+btn19.addEventListener('click', () => {
+  img.src = './images/'
+  document.getElementById('result').innerHTML = "Qorboboni yomon bolalar ro'yxatiga kirish.";
+});
+btn20.addEventListener('click', () => {
+  img.src = './images/MACbook.png'
+  document.getElementById('result').innerHTML = "";
+});
