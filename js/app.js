3cd4ee2b5895b1a81b90cb8dff5363a4cfcb0348
Muhammad5776 <72851181+Muhammad5776@users.noreply.github.com>
2020-12-01 20:23:38 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// $.snowfall.start({
+//   content: '&#10052',
+//   interval: 60
+// });
+var countDownDate = new Date('Jan 1, 2021 00:00:00').getTime();
+var x = setInterval(function(){
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60* 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60 )) / 1000);
+  document.getElementById('dayOutput').innerHTML = days 
+  document.getElementById('hoursOutput').innerHTML = hours 
+  document.getElementById('minutesOutput').innerHTML = minutes 
+  document.getElementById('secondsOutput').innerHTML = seconds 
+})
